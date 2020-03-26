83e743b2ed50d5472eb38eeff54b096bb53e2ed8
Gabriel <gabriel.developer@outlook.com>
2020-03-26 00:41:40 -0300

    etendendo melhor o express

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const express = require('express')
-     ,load = require('express-load')
-     ,app = express();
-
+var express = require('express'),
+  routes = require('./routes');
+var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.statis(__dirname + '/public'));
-
+app.use(express.static(__dirname + '/public'));
 app.get('/', routes.index);
-app.get('/usuarios', routes.unsubscribe.index);
+app.get('/usuarios', routes.user.index);
+app.listen(3000, function () {
+  console.log("Ntalk no ar.");
+});
 
-load
+load('models')
+  .then('controllers')
+  .then(routes)
+  .into(app)
+
 
 app.listen(3000, () => console.log('Ntalk no ar'));
---
+++ routes/home.js
@@ -0,0 +1,4 @@
+module.exports = app => {
+    const home = app.controllers.home;
+    app.get('/', home.index);
+}
